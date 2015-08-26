7a17dc0aae95e812c3500b1468ac86f09e2faba2
jarr416 <jarr416@gmail.com>
2015-08-25 19:05:56 -0600

    Get gh-pages working

--- js/names.js
+++ js/names.js
@@ -36,44 +36,6 @@ var imported = false;
 // }
 // ];
 
-/**
- * Raffle
- * 2012
- * https://github.com/stringham/raffle
- * Copyright Ryan Stringham
- */
-
-//set the colors for each life, in HEX
-var colors = ["#ABC8E4","#628CB6","#003366","#001948","#001948","#001948","#001948"];
-
-/*
- * If you want to have the names show up on the page without entering them into
- * the text field, you can define them here. There is an option to give names
- * points, if you don't then it defaults to one point.
- */
-var imported = false;
-// var imported = [
-// {
-// 	name:'Fred',
-// 	points:2
-// },{
-// 	name:'Dallin',
-// 	points:3
-// },{
-// 	name:'Ryan'
-// },{
-// 	name:'Paul'
-// },{
-// 	name:'Wade'
-// },{
-// 	name:'Kesler'
-// },{
-// 	name:'Brett'
-// },{
-// 	name:'Tyler'
-// }
-// ];
-
 
 /**
  * This supports retrieving a list of names from a published google sheets.
